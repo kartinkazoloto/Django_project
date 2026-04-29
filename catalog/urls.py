ce97fd64c18678ac646060aef495ac24e80645b4
Natalia Zolotova <natakart@inbox.ru>
2026-04-29 23:11:40 +0300

    part1 HW-25

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts, products_list, product_detail
+from catalog.views import HomeView, Contacts, ProductListView, ProductDetailView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('contacts/', contacts, name='contacts'),
-    path('products_list/', products_list, name='products_list'),
-    path('product_detail/<int:product_id>/', product_detail, name='product_detail'),
+    path('', HomeView.as_view(), name='home'),
+    path('contacts/', Contacts.as_view(), name='contacts'),
+    path('product_list/', ProductListView.as_view(), name='product_list'),
+    path('product_detail/<int:product_id>/', ProductDetailView.as_view(), name='product_detail'),
 ]
