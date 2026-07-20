de2b3c5b944670c160ff19e9aac58afde260cb44
Natalia Zolotova <natakart@inbox.ru>
2026-07-20 22:23:06 +0300

    HW_26

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import HomeView, Contacts, ProductListView, ProductDetailView
+from catalog.views import (HomeView, Contacts, ProductListView, ProductDetailView, ProductCreateView,
+                           ProductDeleteView, ProductUpdateView)
+
 
 app_name = CatalogConfig.name
 
@@ -8,5 +10,8 @@ urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('contacts/', Contacts.as_view(), name='contacts'),
     path('product_list/', ProductListView.as_view(), name='product_list'),
-    path('product_detail/<int:product_id>/', ProductDetailView.as_view(), name='product_detail'),
+    path('product_detail/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('product/add/', ProductCreateView.as_view(), name='product_add'),
+    path('product_detail/<int:pk>/edit/', ProductUpdateView.as_view(), name='product_edit'),
+    path('product_detail/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
 ]
--- config/settings.py
+++ config/settings.py
@@ -124,3 +124,9 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+    messages.SUCCESS: 'success',
+}
